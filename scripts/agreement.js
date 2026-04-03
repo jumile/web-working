c76a1e74d85be5624a238831abedcf7787cb0335
jumile <85567621+jumile@users.noreply.github.com>
2026-04-03 14:46:03 +0300

    Delete scripts directory

--- scripts/agreement.js
+++
@@ -1,9 +0,0 @@
-const submitButton = document.querySelector('.form__button');
-const checkbox = document.getElementById('agree');
-
-submitButton.disabled = true;
-
-checkbox.addEventListener('change', () => {
-  if (checkbox.checked) submitButton.disabled = false;
-  else submitButton.disabled = true;
-});
